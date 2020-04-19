9f0138d6ecdcec4c7eb1e1dee20e9902bedc2d64
KHARILIE04 <63888514+KHARILIE04@users.noreply.github.com>
2020-04-19 21:48:49 +0700

    Update subma.php

--- subma.php
+++ subma.php
@@ -60,7 +60,7 @@ echo color("green"," =================================== \n");
 				echo "\n".color("yellow","!] Please wait...");
 				for($a=1;$a<=3;$a++){
 					echo color("yellow",".");
-					sleep(3);
+					sleep(1);sleep(2);sleep(3);
 				}
 				$code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD160420A"}');
 				$message = fetch_value($code1,'"message":"','"');
@@ -74,7 +74,7 @@ echo color("green"," =================================== \n");
 					echo "\n".color("yellow","!] Please wait...");
 					for($a=1;$a<=3;$a++){
 						echo color("yellow",".");
-						sleep(3);
+						sleep(1); sleep(2); sleep(3);
 					}
 					sleep(3);
 					$boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD160420B"}');
@@ -90,7 +90,7 @@ echo color("green"," =================================== \n");
 					echo "\n".color("yellow","!] Please wait...");
 					for($a=1;$a<=3;$a++){
 						echo color("yellow",".");
-						sleep(3);
+						sleep(1); sleep(2); sleep(3);
 					}
 					sleep(3);
 					$goride = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD160420A"}');
@@ -101,13 +101,14 @@ echo color("green"," =================================== \n");
 					echo "\n".color("yellow","!] Please wait...");
 					for($a=1;$a<=3;$a++){
 						echo color("yellow",".");
-						sleep(2);
+						sleep(1); sleep(2); sleep(3);
+P
 					}
 					sleep(3);
 					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD160420B"}');
 					$message2 = fetch_value($goride1,'"message":"','"');
 					echo "\n".color("green","+] Message: ".$message2);
-					sleep(3);
+					sleep(1); sleep(2);
 					
 					$cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=10&page=1', $token);
 					$total = fetch_value($cekvoucher,'"total_vouchers":',',');
